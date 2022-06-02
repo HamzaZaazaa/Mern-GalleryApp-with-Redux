30e1e6bc2e0c9696bb6b355e1290a9f1542a4b1f
HamzaZaazaa <Hamza.zaazaa01@hotmail.com>
2022-06-02 17:35:02 +0100

    heroku pelase

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const connectDb = require("./config/connectDb");
 const auth = require("./auth/auth");
 const profileroute = require("./routes/userprofileroute");
@@ -18,12 +19,13 @@ app.use("/api/post", galleryroute);
 app.use("/api/comments", commentroute);
 app.use("/api/admin", adminroute);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 // app running on port 9000
 const port = process.env.PORT || 9000;
 app.listen(port, (err) => console.log(`app running on port ${port}`));
