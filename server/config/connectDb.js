27daa771d6202c22bb9b0ab08bb0428786088ecb
HamzaZaazaa <Hamza.zaazaa01@hotmail.com>
2022-06-02 16:46:14 +0100

    heroku update

--- server/config/connectDb.js
+++ server/config/connectDb.js
@@ -3,7 +3,10 @@ require("dotenv").config();
 const connectDb = async () => {
   // Connecting database to server...
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(
+      process.env.MONGO_URI ||
+        "mongodb+srv://HamzaZaazaa:hamza0hamza@cluster0.eromy.mongodb.net/MyMernApp?retryWrites=true&w=majority"
+    );
     console.log("DB is connected");
   } catch (error) {
     console.log(error);
