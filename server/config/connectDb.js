5d7aebce0b8fca0fe7c3c93ca6e522704437cbad
HamzaZaazaa <Hamza.zaazaa01@hotmail.com>
2022-06-06 11:33:37 +0100

    uploading

--- server/config/connectDb.js
+++ server/config/connectDb.js
@@ -4,7 +4,7 @@ const connectDb = async () => {
   // Connecting database to server...
   try {
     await mongoose.connect(
-      process.env.MONGO_URI 
+      process.env.MONGO_URI ||"mongodb+srv://HamzaZaazaa:hamza0hamza@cluster0.eromy.mongodb.net/MyMernApp?retryWrites=true&w=majority"
     );
     console.log("DB is connected");
   } catch (error) {
